c6dd5997196c1158af9a078d085f9ad0f4274461
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-10 16:16:01 +0700

    config pack macos app

--- electron/main.js
+++ electron/main.js
@@ -1,27 +1,22 @@
 const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
 const path = require('path');
-const url = require('url');
+
 let mainWindow;
-function createWindow () {
-  const startUrl = process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, '../index.html'),
-    protocol: 'file:',
-    slashes: true,
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width:800,
+    height:600,
+    show: false
   });
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
-  mainWindow.loadURL(startUrl);
-  mainWindow.on('closed', function () {
+  const startURL = isDev ? 'http://localhost:7777' : `file://${path.join(__dirname, '../build/index.html')}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.once('ready-to-show', () => mainWindow.show());
+  mainWindow.on('closed', () => {
     mainWindow = null;
   });
 }
 app.on('ready', createWindow);
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
