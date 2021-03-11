1af2bc909bf3a237cbe0e6df3f35b5c70868e601
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-11 10:48:57 +0700

    test apply JsonView Comp

--- src/containers/JsonFormatter/JsonFormater.js
+++ src/containers/JsonFormatter/JsonFormater.js
@@ -1,7 +1,22 @@
 import React from "react";
+import JsonView from "../../components/JsonFormatter/JsonView/JsonView";
 
 export default () => {
+  const jsonString = JSON.stringify({
+    a: {
+      b: 1,
+      c: 2.1,
+      d: 'test',
+      test: {
+        b: 1,
+        c: 2.1,
+        d: 'test'
+      }
+    },
+    t: '1123',
+    fd: ''
+  }, null, 4);
   return (
-    <h1>test</h1>
+    <JsonView jsonString={jsonString}/>
   );
 };
