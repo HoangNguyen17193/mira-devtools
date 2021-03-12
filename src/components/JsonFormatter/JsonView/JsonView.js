402df3798a30a708ac4ae4c11fdc9083169b3979
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-12 11:59:30 +0700

    custom highlighter

--- src/components/JsonFormatter/JsonView/JsonView.js
+++ src/components/JsonFormatter/JsonView/JsonView.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
-import {materialDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {materialDark, atomDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import classes from './JsonView.module.scss';
 
 const JsonView = (props) => {
-
+  const customMaterialDark = {
+    ...atomDark,
+    "code[class*=\"language-\"]": {
+      ...atomDark["code[class*=\"language-\"]"],
+      "background": "#323841",
+    },
+    "pre[class*=\"language-\"]": {
+      ...atomDark["pre[class*=\"language-\"]"],
+      "background": "#323841",
+      "border-radius": "10px"
+    }
+  }
   return (
     <div className={classes.JsonView}>
-      <SyntaxHighlighter language="json" style={materialDark} showLineNumbers={true}>
+      <SyntaxHighlighter language="json" style={customMaterialDark} showLineNumbers={true}>
         {props.jsonString || ''}
       </SyntaxHighlighter>
     </div>
