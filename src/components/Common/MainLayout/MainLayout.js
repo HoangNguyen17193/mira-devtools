36dd591c525cc824e93002caa575ca96ec57561b
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-16 16:10:47 +0700

    update icons, nav size

--- src/components/Common/MainLayout/MainLayout.js
+++ src/components/Common/MainLayout/MainLayout.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react'
 import {NavLink, withRouter} from "react-router-dom";
 import {Layout, Menu} from 'antd';
-import {HeartFilled} from '@ant-design/icons'
 import 'antd/dist/antd.css';
 import classes from './MainLayout.module.scss';
 import {ROUTES} from '../../../constants';
-import {FundOutlined, CloudServerOutlined, ToolOutlined, ControlOutlined} from '@ant-design/icons';
+import {
+  CodeOutlined,
+  ClockCircleOutlined,
+  HeartFilled,
+  UnlockOutlined,
+  UndoOutlined
+} from '@ant-design/icons';
 import './MainLayout.scss';
-
 const {Header, Content, Footer, Sider} = Layout;
 
 class MainLayout extends Component {
@@ -31,21 +35,26 @@ class MainLayout extends Component {
           Mira Devtools
         </Header>
         <Layout className="site-layout MainLayout">
-          <Sider width={230} collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          <Sider width={200} collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
             <Menu theme="dark" mode="inline" selectedKeys={[key]}>
-              <Menu.Item icon={<CloudServerOutlined/>} key={ROUTES.home.key}>
+              <Menu.Item icon={<CodeOutlined/>} key={ROUTES.home.key}>
                 <NavLink exact to={ROUTES.home.path}>
                   JSON Parser
                 </NavLink>
               </Menu.Item>
-              <Menu.Item icon={<FundOutlined/>} key="">
+              <Menu.Item icon={<UnlockOutlined/>} key="">
                 <NavLink to="">
                   JWT Parser
                 </NavLink>
               </Menu.Item>
-              <Menu.Item icon={<ToolOutlined/>} key="">
+              <Menu.Item icon={<UndoOutlined/>} key="">
                 <NavLink to="">
                   UUID generator
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item icon={<ClockCircleOutlined/>} key="">
+                <NavLink to="">
+                  UnixTime Converter
                 </NavLink>
               </Menu.Item>
             </Menu>
