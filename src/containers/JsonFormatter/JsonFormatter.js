8011423af3bf86cc7b374ab5af3fcf735fb093b2
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-17 13:57:52 +0700

    apply split view

--- src/containers/JsonFormatter/JsonFormatter.js
+++ src/containers/JsonFormatter/JsonFormatter.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Row, Col, Input } from 'antd';
 import { debounce } from 'lodash';
 
+import Split from 'react-split'
+
 import JsonView from "../../components/JsonFormatter/JsonView/JsonView";
 import classes from './JsonFormatter.module.scss';
 import './JsonFormatter.scss';
@@ -16,14 +18,23 @@ export default () => {
   }, 400);
 
   return (
-    <Row className={`JsonFormatter ${classes.JsonFormatter}`}>
-      <Col className={classes.InputContainer} span={12}>
-        <TextArea onChange={onInputChanged} className={classes.Input} />
-      </Col>
-      <Col className={classes.JsonViewContainer} span={12}>
-        <JsonView jsonString={jsonString}/>
-      </Col>
-    </Row>
+    <div className={`JsonFormatter ${classes.JsonFormatter}`}>
+      <Split className={classes.SplitContainer}
+             sizes={[50, 50]}
+             cursor="col-resize"
+             gutterSize={10}
+             minSize={300}
+             expandToMin={true}
+      >
+        <div className={classes.InputContainer} span={12}>
+          <TextArea onChange={onInputChanged} className={classes.Input} />
+        </div>
+        <div className={classes.JsonViewContainer} span={12}>
+          <JsonView jsonString={jsonString}/>
+        </div>
+      </Split>
+
+    </div>
 
   );
 };
