f98d39ac6a7605fe4068f2cbc6e6634dbdc233ad
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-19 13:31:29 +0700

    JWT Parser

--- src/containers/JWTParser/JWTParser.js
+++ src/containers/JWTParser/JWTParser.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { Input } from 'antd';
 
-export default (props) => {
-  return (<div>
-    <h1>JWT Parser</h1>
-  </div>);
+import SplitContainer from '../../components/Common/SplitContainer/SplitContainer';
+import classes from './JWTParser.module.scss';
+import './JWTParser.scss';
+import {setToken} from '../../store/JWTParser/JWTParserAction';
+import JWtParserResult from '../../components/JWTParser/JWTParserResult/JWTParserResult';
+
+const {TextArea} = Input;
+
+export default () => {
+  const token = useSelector(state => state.JWTParser.token);
+  const dispatch = useDispatch();
+
+  const onInputChanged = (e) => {
+    dispatch(setToken(e.target.value));
+  }
+
+  return (
+    <div className={`${classes.JWTParser} JWTParser`}>
+      <SplitContainer>
+        <div className={classes.InputContainer}>
+          <TextArea placeholder='Paste a token here' value={token} onChange={onInputChanged} className={classes.Input} />
+        </div>
+        <div className={classes.JWTResultContainer}>
+          <JWtParserResult token={token}/>
+        </div>
+      </SplitContainer>
+
+    </div>
+  );
 }
