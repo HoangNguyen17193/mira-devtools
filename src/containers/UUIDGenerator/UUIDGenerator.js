9f30f142dd45244f3d3176004ad536550f7b55de
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-19 17:02:40 +0700

    UUID Generator

--- src/containers/UUIDGenerator/UUIDGenerator.js
+++ src/containers/UUIDGenerator/UUIDGenerator.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import {Col, Row} from 'antd';
+import {useSelector, useDispatch} from 'react-redux';
 
-import UUIDCard from "../../components/UUIDGenerator/UUIDCard/UUIDCard";
+import UUIDCard from '../../components/UUIDGenerator/UUIDCard/UUIDCard';
 import classes from './UUIDGenerator.module.scss';
+import {generateUUIDV1, generateUUIDV4} from '../../store/UUIDGenerator/UUIDGeneratorAction';
 
 export default () => {
-
+  const uuidV1Value = useSelector(state => state.UUIDGenerator.uuidV1Value);
+  const uuidV4Value = useSelector(state => state.UUIDGenerator.uuidV4Value);
+  const dispatch = useDispatch();
   return (
     <div className={classes.UUIDGenerator}>
       <Row className={classes.UUIDGeneratorRow}>
         <Col span={12}>
           <UUIDCard
             title='UUID v1(timestamp)'
+            randomString={uuidV1Value}
+            generate={() => dispatch(generateUUIDV1())}
           />
         </Col>
         <Col span={12}>
           <UUIDCard
-            title='UUID v3(namespace w/ MD5)'
+            title='UUID v4(random)'
+            randomString={uuidV4Value}
+            generate={() => dispatch(generateUUIDV4())}
           />
         </Col>
-        <Col span={12}>
-          <UUIDCard
-            title='UUID v4(random)'/>
-        </Col>
-        <Col span={12}>
-          <UUIDCard
-            title='UUID v5(namespace w/ SHA-1)'/>
-        </Col>
-
       </Row>
-
     </div>);
 }
