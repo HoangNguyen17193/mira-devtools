1aa2698bcb526d97ee7a1b7acb6da50428f13c28
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-19 16:27:48 +0700

    UUID Generator ui

--- src/containers/UUIDGenerator/UUIDGenerator.js
+++ src/containers/UUIDGenerator/UUIDGenerator.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import {Col, Row} from 'antd';
+
+import UUIDCard from "../../components/UUIDGenerator/UUIDCard/UUIDCard";
+import classes from './UUIDGenerator.module.scss';
 
 export default () => {
-  return (<div>
-    <h1>UUID Generator</h1>
-  </div>);
+
+  return (
+    <div className={classes.UUIDGenerator}>
+      <Row className={classes.UUIDGeneratorRow}>
+        <Col span={12}>
+          <UUIDCard
+            title='UUID v1(timestamp)'
+          />
+        </Col>
+        <Col span={12}>
+          <UUIDCard
+            title='UUID v3(namespace w/ MD5)'
+          />
+        </Col>
+        <Col span={12}>
+          <UUIDCard
+            title='UUID v4(random)'/>
+        </Col>
+        <Col span={12}>
+          <UUIDCard
+            title='UUID v5(namespace w/ SHA-1)'/>
+        </Col>
+
+      </Row>
+
+    </div>);
 }
