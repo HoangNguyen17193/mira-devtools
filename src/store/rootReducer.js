9d50b17a99bd2e29a39be00b217167efc088585d
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-19 17:02:15 +0700

    UUID Generator store

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import JSONFormatterReducer from './JSONFormatter/JSONFormatterReducer';
 import JWTParserReducer from './JWTParser/JWTParserReducer';
+import UUIDGeneratorReducer from './UUIDGenerator/UUIDGeneratorReducer';
 
 export default combineReducers({
   JSONFormatter: JSONFormatterReducer,
   JWTParser: JWTParserReducer,
+  UUIDGenerator: UUIDGeneratorReducer
 });
