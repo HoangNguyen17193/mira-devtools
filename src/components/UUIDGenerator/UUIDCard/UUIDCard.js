739e198586cf7be39ce75d6479ce5e94c64cf4ee
Hoang Nguyen <hoangnh17193@gmail.com>
2021-03-22 11:37:28 +0700

    add copy

--- src/components/UUIDGenerator/UUIDCard/UUIDCard.js
+++ src/components/UUIDGenerator/UUIDCard/UUIDCard.js
@@ -6,13 +6,16 @@ import classes from './UUIDCard.module.scss';
 import './UUIDCard.scss';
 
 export default (props) => {
+  const copy = () => {
+    navigator.clipboard.writeText(props.randomString);
+  };
   return (
     <Card className={`${classes.UUIDCard} UUIDCard`} title={props.title} bordered={false}>
       <Input disabled className={classes.Input} value={props.randomString} />
       <Button type="primary" shape="round" icon={<RedoOutlined />} onClick={props.generate}>
         Generate
       </Button>
-      <Button className={classes.Button} type="default" shape="round" icon={<CopyOutlined />}>
+      <Button className={classes.Button} type="default" shape="round" icon={<CopyOutlined />} onClick={copy}>
         Copy
       </Button>
     </Card>
